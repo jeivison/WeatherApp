d0a5c4adf7558dc48353e3588051bbf9dda112dd
jeivi <jeivison1@hotmail.com>
2022-02-13 23:52:55 -0300

    project started!

--- app/src/main/java/com/example/weatherapp/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/weatherapp/viewmodel/MainViewModel.kt
@@ -1,11 +1,16 @@
 package com.example.weatherapp.viewmodel
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.example.weatherapp.model.WeatherModel
 import com.example.weatherapp.service.WeatherAPIService
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.ScheduledFuture
 
-class MainViewModel {
+class MainViewModel: ViewModel(){
 
     private val weatherAPIService = WeatherAPIService()
     private val disposable = CompositeDisposable()
@@ -20,7 +25,26 @@ class MainViewModel {
     }
 
     private fun getDataFromAPI() {
-        TODO("Not yet implemented")
+
+        waether_load.value = true
+        disposable.add(
+            weatherAPIService.getDataService()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object: DisposableSingleObserver<WeatherModel>(){
+                    override fun onSuccess(t: WeatherModel) {
+                        waether_data.value = t
+                        waether_error.value = false
+                        waether_load.value = false
+                    }
+
+                    override fun onError(e: Throwable) {
+                        waether_error.value = true
+                        waether_load.value = false
+                    }
+
+                })
+        )
     }
 
 }
