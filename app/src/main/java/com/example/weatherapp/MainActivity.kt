4bab389e95ca34fe75e5efe93da5e19760ffa3a8
jeivison <jeivison1@hotmail.com>
2022-03-07 04:59:40 -0300

    project started6!

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -8,16 +8,18 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.weatherapp.databinding.ActivityMainBinding
 import com.example.weatherapp.main.ViewModelFactory
 import com.example.weatherapp.main.WeatherViewModel
+import com.example.weatherapp.model.Forecast
+import com.example.weatherapp.model.WeatherData
 import com.example.weatherapp.repository.MainRepository
-
-
+import org.w3c.dom.Text
+import kotlin.math.max
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private lateinit var viewmodel : WeatherViewModel
+    private lateinit var viewModel: WeatherViewModel
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,19 +29,31 @@ class MainActivity : AppCompatActivity() {
 
         val repository = MainRepository()
         val viewModelFactory = ViewModelFactory(repository)
-        viewmodel = ViewModelProvider(this, viewModelFactory).get(WeatherViewModel::class.java)
-        viewmodel.getWeather()
+        viewModel = ViewModelProvider(this, viewModelFactory).get(WeatherViewModel::class.java)
+        viewModel.getWeather()
         getLiveData()
 
         }
 
 
     private fun getLiveData() {
-        viewmodel.resultsLiveData.observe(this) { resultado ->
+        viewModel.resultsLiveData.observe(this) { resultado ->
             binding.apply {
-                address.text = resultado.results.cityName
-                temp.text = resultado.results.temp.toString()
-                //Log.d("Resultado", resultado)
+                address.text = resultado.results.city
+                updatedAt.text = resultado.results.date
+                temp.text = "${resultado.results.temp}°C"
+                status.text = resultado.results.description
+                sunrise.text = resultado.results.sunrise
+                sunset.text = resultado.results.sunset
+                wind.text = resultado.results.windSpeedy
+                pressure.text = resultado.results.imgId
+                humidity.text = resultado.results.humidity.toString()
+
+                val forecast1 = resultado.results.forecast[0]
+                val forecast2 = resultado.results.forecast[1]
+                tempMax.text = "Max: ${forecast1.max}"
+                tempMin.text = "Min: ${forecast2.min}"
+
             }
         }
     }
--- app/src/main/java/com/example/weatherapp/service/ServiceAPI.kt
+++ app/src/main/java/com/example/weatherapp/service/ServiceAPI.kt
@@ -5,7 +5,6 @@ import com.example.weatherapp.model.WeatherData
 import retrofit2.http.GET
 
 interface ServiceAPI {
-    //https://api.hgbrasil.com/weather?woeid=455827
     //https://api.hgbrasil.com/weather?key=a30a6ec9
 
     @GET("/weather?key=a30a6ec9")
